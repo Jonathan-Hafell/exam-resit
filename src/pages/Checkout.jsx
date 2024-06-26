eb9253c7aae428d7291c7071bbec1a766871c127
Jonathan Hafell <Jonathan.hafell@gmail.com>
2024-06-26 17:28:01 +0200

    Added relevant checkout form validation

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+/*import { useState, useEffect } from "react";
 import { getCart } from "../utils/getCart";
 import { Modal, Button } from "react-bootstrap";
 import "../styles/Checkout.scss";
@@ -381,6 +381,319 @@ const CheckoutPage = () => {
             Cancel
           </Button>
           <Button variant="primary" onClick={handleModalConfirm}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default CheckoutPage;*/
+
+import { useState, useEffect, useRef } from "react";
+import { getCart } from "../utils/getCart";
+import BreadcrumbComponent from "../components/Breadcrumb";
+import { Container, Row, Col, Form, Button, Modal } from "react-bootstrap";
+import "../styles/Checkout.scss";
+
+const CheckoutPage = () => {
+  const [cartItems, setCartItems] = useState([]);
+  const [validated, setValidated] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const cart = getCart();
+    setCartItems(cart);
+  }, []);
+
+  const breadcrumbs = [
+    { label: "Browse", path: "/browse", active: false },
+    { label: "Cart", path: "/cart", active: false },
+    { label: "Checkout", path: "/checkout", active: true },
+  ];
+
+  const handleSubmit = (event) => {
+    const form = formRef.current;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      event.preventDefault();
+      setShowModal(true);
+    }
+    setValidated(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const confirmPayment = () => {
+    localStorage.removeItem("cart");
+    setShowModal(false);
+    window.location.href = "/browse";
+  };
+
+  return (
+    <div className="checkout-page">
+      <BreadcrumbComponent breadcrumbs={breadcrumbs} />
+      <Container>
+        <Row className="g-5">
+          <Col md={5} lg={4} className="order-md-last">
+            <h4 className="d-flex justify-content-between align-items-center mb-3">
+              <span className="text-primary">Your cart</span>
+              <span className="badge bg-primary rounded-pill">
+                {cartItems.length}
+              </span>
+            </h4>
+            <ul className="list-group mb-3">
+              {cartItems.map((item) => (
+                <li
+                  key={item.id}
+                  className="list-group-item d-flex justify-content-between lh-sm"
+                >
+                  <div>
+                    <h6 className="my-0">{item.attributes.title}</h6>
+                    <small className="text-body-secondary">
+                      {item.attributes.description}
+                    </small>
+                  </div>
+                  <span className="text-body-secondary">
+                    ${item.attributes.price}
+                  </span>
+                </li>
+              ))}
+              <li className="list-group-item d-flex justify-content-between">
+                <span>Total (NOK)</span>
+                <strong>200</strong>
+              </li>
+            </ul>
+          </Col>
+          <Col md={7} lg={8}>
+            <h4 className="mb-3">Billing address</h4>
+            <Form
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+              ref={formRef}
+            >
+              <Row className="g-3">
+                <Col sm={6}>
+                  <Form.Group controlId="firstName">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="[A-Za-z]+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Valid first name is required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col sm={6}>
+                  <Form.Group controlId="lastName">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="[A-Za-z]+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Valid last name is required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={12}>
+                  <Form.Group controlId="email">
+                    <Form.Label>Email (Optional)</Form.Label>
+                    <Form.Control type="email" placeholder="you@example.com" />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter a valid email address for shipping updates.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={12}>
+                  <Form.Group controlId="address">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder="1234 Main St"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter your shipping address.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={5}>
+                  <Form.Group controlId="country">
+                    <Form.Label>Country</Form.Label>
+                    <Form.Select required>
+                      <option value="">Choose...</option>
+                      <option>Norway</option>
+                      <option>Sweden</option>
+                      <option>Denmark</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                      Please select a valid country.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={4}>
+                  <Form.Group controlId="state">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="[A-Za-z]+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid city.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={3}>
+                  <Form.Group controlId="zip">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="\d+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Zip code required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Form.Group className="form-check my-4">
+                <Form.Check
+                  type="checkbox"
+                  id="save-info"
+                  label="Save this information for next time"
+                />
+              </Form.Group>
+
+              <hr className="my-4" />
+
+              <h4 className="mb-3">Payment</h4>
+
+              <Form.Group className="my-3">
+                <Form.Check
+                  type="radio"
+                  label="Credit card"
+                  name="paymentMethod"
+                  id="credit"
+                  required
+                />
+                <Form.Check
+                  type="radio"
+                  label="Debit card"
+                  name="paymentMethod"
+                  id="debit"
+                  required
+                />
+                <Form.Check
+                  type="radio"
+                  label="PayPal"
+                  name="paymentMethod"
+                  id="paypal"
+                  required
+                />
+              </Form.Group>
+
+              <Row className="gy-3">
+                <Col md={6}>
+                  <Form.Group controlId="cc-name">
+                    <Form.Label>Name on card</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="[A-Za-z\s]+"
+                    />
+                    <small className="text-body-secondary">
+                      Full name as displayed on card
+                    </small>
+                    <Form.Control.Feedback type="invalid">
+                      Name on card is required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group controlId="cc-number">
+                    <Form.Label>Card number</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="\d+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Card number is required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={3}>
+                  <Form.Group controlId="cc-expiration">
+                    <Form.Label>Expiration</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="\d+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Expiration date required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={3}>
+                  <Form.Group controlId="cc-cvv">
+                    <Form.Label>CVV</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder=""
+                      pattern="\d+"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Security code required.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <hr className="my-4" />
+
+              <Button className="w-100 btn-lg" type="submit">
+                Continue to checkout
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Payment Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to proceed with the payment?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={confirmPayment}>
             Confirm
           </Button>
         </Modal.Footer>
