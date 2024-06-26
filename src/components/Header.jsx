4584c19993caa6c7f70f80b1061169d53777ae1c
Jonathan Hafell <Jonathan.hafell@gmail.com>
2024-06-26 18:08:59 +0200

    Header optimization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,16 @@ import { useAuth } from "../api/auth/useAuth";
 
 const Header = () => {
   const { isLoggedIn, logout } = useAuth();
+
   return (
     <nav
       className="navbar navbar-expand-sm navbar-dark bg-dark"
       aria-label="Third navbar example"
     >
       <div className="container-fluid px-5">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to={isLoggedIn ? "/browse" : "/"}>
           Bits & Bots
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -26,21 +27,25 @@ const Header = () => {
 
         <div className="collapse navbar-collapse" id="navbarsExample03">
           <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/services">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
+            {!isLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/services">
+                    Services
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </>
+            )}
             {isLoggedIn && (
               <>
                 <li className="nav-item">
